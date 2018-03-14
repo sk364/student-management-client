8c2fc74d50b08216f1b69c363e2f47264ced21d5
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2018-03-14 12:38:45 +0530

    Integrated bootstrap to login component

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,71 +1,104 @@
 import React, { Component } from 'react';
 import WebStorage from '../../webstorage';
 import Config from 'react-global-configuration';
+import { Button, FormGroup, FormControl, ControlLabel, PageHeader } from "react-bootstrap";
 import { fetchWithHeaders } from '../../helper';
+import './login.css';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loginError: '',
+      username: '',
+      password: '',
+      loginError: ''
     };
   }
 
   /**
-   * @desc Handles login form submission
+   * @desc Submits login form to the API server
+   * @param `email` Email of the user
+   * @param `password` Password of the user
    *
    */
-   loginSubmitHandle = (e) => {
-     e && e.preventDefault();
+  submitLoginForm = (event) => {
+    event.preventDefault();
 
-     var form = e.nativeEvent.target,
-         email = form.email.value,
-         password = form.password.value;
+    var formData = JSON.stringify({
+      'username': this.state.username,
+      'password': this.state.password
+    });
 
-     this.submitLoginForm(email, password);
-   }
+    fetchWithHeaders('/login/', 'POST', null, formData).then((response) => {
+      if (response.token) {
+        WebStorage.setItem('_jwt', response.token);
+        WebStorage.setItem('isUserLoggedIn', true);
+        WebStorage.setItem('isAdmin', response.is_admin);
+        WebStorage.setItem('userId', response.user_id);
+        Config.update('isUserLoggedIn', true);
+        Config.update('isAdmin', response.is_admin);
+        Config.update('userId', response.user_id);
+        this.props.history.push('/');
+      } else {
+        this.setState({
+          password: '',
+          loginError: response.non_field_errors
+        });
+      }
+    });
+  }
 
-   /**
-    * @desc Submits login form to the API server
-    * @param `email` Email of the user
-    * @param `password` Password of the user
-    *
-    */
-   submitLoginForm = (email, password) => {
-     var formData = JSON.stringify({
-       'username': email,
-       'password': password
-     });
+  /**
+   * @desc Handles input change
+   *
+   */
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
 
-     fetchWithHeaders('/login/', 'POST', null, formData).then((response) => {
-       if (response.token) {
-         WebStorage.setItem('_jwt', response.token);
-         WebStorage.setItem('isUserLoggedIn', true);
-         WebStorage.setItem('isAdmin', response.is_admin);
-         WebStorage.setItem('userId', response.user_id);
-         Config.update('isUserLoggedIn', true);
-         Config.update('isAdmin', response.is_admin);
-         Config.update('userId', response.user_id);
-         this.props.history.push('/');
-       } else {
-         var form = document.body.querySelector('form');
-         form.password.value = '';
-         this.setState({
-           loginError: 'Invalid Email/Password.'
-         });
-       }
-     });
-   }
+  /**
+   * @desc Validates form
+   *
+   */
+  validateForm = () => {
+    return this.state.username.length > 0 && this.state.password.length > 0;
+  }
 
-  render() {
+  render = () => {
     return(
-      <div>
-        <form onSubmit={this.loginSubmitHandle}>
-          <input type="email" name="email" required="true" />
-          <input type="password" name="password" required="true" />
-          <button type="submit">Login</button>
-          { this.state.loginError }
+      <div className="Login">
+        <center><PageHeader>Login</PageHeader></center>
+        <form onSubmit={this.submitLoginForm}>
+          <FormGroup controlId="username" bsSize="large">
+            <ControlLabel>Username</ControlLabel>
+            <FormControl
+              autoFocus
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
         </form>
+
+        <div className="login-error">{ this.state.loginError }</div>
       </div>
     );
   }
